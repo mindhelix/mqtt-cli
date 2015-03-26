ff65b48982817af0aadded515b8e689162cd7a93
Jinsu Mathew <jinsu@mindhelix.com>
2015-03-26 13:49:22 +0530

    Adds option to run as deamon

--- nmbl.js
+++ nmbl.js
@@ -8,7 +8,8 @@
 var mqtt    = require('mqtt');
 
 if (process.argv[2] == '--help' || process.argv[2] == '-h') {
-  console.log('Usage: nmbl hostname topic payload \ne.g. nmbl smartsocket.getmyrico.com SOC101 toggle:SOC110:0:on');
+  console.log('Usage: node nmbl hostname topic payload \n\te.g. node nmbl smartsocket.getmyrico.com SOC101 toggle:SOC110:0:on');
+  console.log('Options: \n\t-h : this help option\n\t-d : pass at the end to run as deamon. Usage: node nmbl hostname topic payload -d');
   process.exit(0);
 }
 
@@ -20,6 +21,7 @@ if (process.argv[2] == '--version' || process.argv[2] == '-v') {
 var client  = mqtt.connect('mqtt://' + process.argv[2]);
 var topic = process.argv[3];
 var payload = process.argv[4];
+var deamonOption = process.argv[5];
 
 client.subscribe(topic);
 client.publish(topic, payload);
@@ -28,4 +30,7 @@ client.on('message', function (topic, message) {
   console.log('Topic: ' + topic + ' Message: ' + message.toString());
 });
 
-client.end();
+
+if ( ! deamonOption || deamonOption !== '-d') {
+  client.end();
+}
